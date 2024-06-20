e6684be0a6fe54c343588d5ca26e2c4b114ef01b
QuietSeeker <123584613+QuietSeeker@users.noreply.github.com>
2024-06-20 15:51:11 +0100

    added 3(c) and 3(d)

--- 2022/q3.py
+++ 2022/q3.py
@@ -1,4 +1,4 @@
-def ways(order,currPrefList): # nmber of pref lists that start with this one and get to the order
+def ways(order,currPrefList): # number of pref lists that start with this one and get to the order
     allCars = sorted(order)
     
     # check currPrefList
@@ -62,3 +62,66 @@ print(solve(order,arrangement))
 #     assert not x in seen
 #     prev = x
 #     seen.add(x)
+
+# 3 (c)
+# If there are only two possible preference lists then given such arrangement one car has two possible
+# preferences, and all the rest parked in their preffered spots.
+#
+# Note that the preferred spot of a car will always be equal to, or to the left of its final spot.
+# So if the car on our left came after us, we are in our preffered spot.
+#
+# The car with two possible preffered spots must be directly to the right of a car came before it.
+# If this car is 'a' then we have 0 possible arrangements. ('a' always gets its preference)
+# If it is 'b' then we have 1 possible arrangement. (pon..cab)
+# If it is 'c' then we have 2 possible arrangements. (pon..dacb, pon..dbca)
+# ...
+# If it is 'p' then we have 15 possible arrangements. 
+#
+# The total number of arrangements is 0 + 1 + ... + 15 = 120
+
+# 3 (d)
+# We modify the counting function
+def waysD(order,currPrefs): # nmber of pref lists that start with these one and get to the order
+    if len(set(currPrefs.values())) != len(currPrefs.keys()):
+        return 0
+
+    allCars = sorted(order)
+    theseCars = set(currPrefs.keys())
+    otherCars = set(allCars) - set(currPrefs.keys())
+
+    # check currPrefList
+    for workingCar in theseCars:
+        finalSpot = order.index(workingCar)
+        preferredSpot = ord(currPrefs[workingCar])-65
+
+        if finalSpot != preferredSpot:
+            return 0
+
+    prod = 1
+
+    for workingCar in otherCars:
+        finalSpot = order.index(workingCar)
+        firstAvailable = finalSpot
+        while firstAvailable >= 1:
+            if order[firstAvailable-1] > workingCar: # the previous spot was empty
+                break
+            firstAvailable -= 1
+        opts = finalSpot - firstAvailable # we cant include the one it parked at as an option due to the new constraints
+        prod *= opts
+    
+    return prod
+
+from itertools import combinations,product
+CARS = "abcdefghijklmnop"
+PREFS = "ABCDEFGHIJKLMNOP"
+rightPlaces = [('a',) + cs for cs in combinations(CARS[1:],2)] # 'a' is always in its prefered spot
+t = list(product(PREFS,repeat=3))
+
+s = 0
+for cars in rightPlaces:
+    for v in t:
+        check = dict(zip(cars,v))
+        s += waysD(CARS,check)
+print(f"{s:,}")
+
+# This computation gives us the answer 6,165,817,614,720
